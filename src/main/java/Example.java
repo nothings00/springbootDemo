936ee5294bbbaba01021aa941165becfe1d77806
nothings00 <19930523guiok>
2018-11-09 22:15:27 +0800

    初始化

---
+++ src/main/java/Example.java
@@ -0,0 +1,20 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by zenghh on 2018-08-28.
+ */
+@RestController
+@EnableAutoConfiguration
+public class Example {
+    @RequestMapping("/")
+    String home(){
+        return "hello world!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Example.class,args);
+    }
+}
