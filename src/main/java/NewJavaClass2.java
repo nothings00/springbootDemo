ca4650fbd1bb5e291fe860b101be1c293cb91bfa
kylekwan1993 <kylekwan@gmail.com>
2018-11-13 23:23:02 +0800

    kyle commit test..20181113

---
+++ src/main/java/NewJavaClass2.java
@@ -0,0 +1,12 @@
+public class NewJavaClass2 extends NewJavaClass {
+
+    private String newString;
+
+    public void setNewString(String newString) {
+        this.newString = newString;
+    }
+
+    public String getNewString() {
+        return newString;
+    }
+}
